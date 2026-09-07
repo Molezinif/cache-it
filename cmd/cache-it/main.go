5efecd175ecd28ac7a4512886fceba521ebe7daa
Gabriel Molezini Ferreira <gabrielmolezinif@gmail.com>
2026-09-06 21:49:53 -0300

    refactor: ping pong but connection with redis-cli RESP

--- cmd/cache-it/main.go
+++ cmd/cache-it/main.go
@@ -7,6 +7,24 @@ import (
 	"net"
 )
 
+func handleConnection(c net.Conn) {
+	defer c.Close()
+
+	scanner := bufio.NewScanner(c)
+
+	for scanner.Scan() {
+		input := scanner.Text()
+
+		if input == "PING" {
+			c.Write([]byte("+PONG\r\n"))
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Erro na conexão: %v\n", err)
+	}
+}
+
 func main() {
 	l, err := net.Listen("tcp", ":6380")
 	if err != nil {
@@ -18,25 +36,9 @@ func main() {
 	for {
 		conn, err := l.Accept()
 		if err != nil {
-			log.Fatal(err)
+			fmt.Println(err)
 		}
 
-		go func(c net.Conn) {
-			defer c.Close()
-
-			scanner := bufio.NewScanner(c)
-
-			for scanner.Scan() {
-				input := scanner.Text()
-
-				if input == "PING" {
-					c.Write([]byte("PONG\n"))
-				}
-			}
-
-			if err := scanner.Err(); err != nil {
-				fmt.Printf("Erro na conexão: %v\n", err)
-			}
-		}(conn)
+		go handleConnection(conn)
 	}
 }
